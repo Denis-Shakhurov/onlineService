0d44dba609949553c0ffc3f094e3b5a54a041e46
DenisShakhurov <disa.sdf86@gmail.com>
2025-01-28 10:17:16 +0700

    #REF refactoring DB connection

--- src/main/java/org/example/config/MySessionFactory.java
+++ src/main/java/org/example/config/MySessionFactory.java
@@ -4,40 +4,27 @@ import org.example.model.Order;
 import org.example.model.Service;
 import org.example.model.User;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
-
-import java.util.Properties;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.boot.registry.StandardServiceRegistry;
 
 public class MySessionFactory {
-    private final String DIALECT_KEY = "db.dialect";
-    private final String DRIVER_KEY = "db.driver";
-    private final String URL_KEY = "db.url";
-    private final String USER_KEY = "db.user";
-    private final String PASSWORD_KEY = "db.password";
-    private SessionFactory sessionFactory;
-    private SessionConfig sessionConfig;
+    private final String APPLICATION_PROPERTIES = "application.properties";
+
 
     public SessionFactory getSessionFactory() {
-        sessionConfig = new SessionConfig();
-        Properties properties = sessionConfig.getProperties();
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .loadProperties(APPLICATION_PROPERTIES)
+                .build();
 
-        properties.put(Environment.DIALECT, sessionConfig.getProperty(DIALECT_KEY));
-        properties.put(Environment.DRIVER, sessionConfig.getProperty(DRIVER_KEY));
-        properties.put(Environment.URL, sessionConfig.getProperty(URL_KEY));
-        properties.put(Environment.USER, sessionConfig.getProperty(USER_KEY));
-        properties.put(Environment.PASS, sessionConfig.getProperty(PASSWORD_KEY));
-        properties.put(Environment.SHOW_SQL, true);
-        properties.put(Environment.HBM2DDL_AUTO, "validate");
-        properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-
-        sessionFactory = new Configuration()
-                .setProperties(properties)
+        Metadata metadata = new MetadataSources(registry)
                 .addAnnotatedClass(User.class)
-                .addAnnotatedClass(Service.class)
                 .addAnnotatedClass(Order.class)
-                .buildSessionFactory();
+                .addAnnotatedClass(Service.class)
+                .getMetadataBuilder()
+                .build();
 
-        return sessionFactory;
+        return metadata.getSessionFactoryBuilder().build();
     }
 }
